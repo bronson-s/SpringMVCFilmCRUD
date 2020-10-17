0aad487ceefbe1f0054778ee2c6b3ef37fa806f4
Chris Hiles <chrishiles790@gmail.com>
2020-10-17 10:06:50 -0600

    new change to controller

--- src/main/java/com/skilldistillery/films/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/films/controllers/FilmController.java
@@ -1,14 +1,26 @@
 package com.skilldistillery.films.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.skilldistillery.film.database.DatabaseAccessor;
+
 @Controller
 public class FilmController {
-	@RequestMapping(path="index.do", method=RequestMethod.GET)
-	  public ModelAndView index() {
-	    ModelAndView mv = new ModelAndView();
-	    mv.setViewName("WEB-INF/home.jsp");
-	    return mv;
-	  }
+	
+	@Autowired
+	private DatabaseAccessor databaseaccessor;
+	
+	public void DatabaseAccessor(DatabaseAccessor databaseaccessor) {
+		this.databaseaccessor = databaseaccessor;
+	}
+	@RequestMapping(path = "index.do", method = RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("WEB-INF/home.jsp");
+		return mv;
+	}
 }
